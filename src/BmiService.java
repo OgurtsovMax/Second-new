75e30ca8b8dcb09e5eee4b561c824a3ceb516dde
Максим Огурцов <Ogurtsov.max@gmail.com>
2023-07-25 12:25:23 +0300

    change

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,8 @@
 public class BmiService {
-    public int calculate(int m, double h) {
+    public int calculate(int weight, double height) {
         double index;
 
-        index = m / (h * h);
+        index = weight/ (height * height);
 
         return (int) index;
 
